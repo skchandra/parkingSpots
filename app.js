a7a72c24c073052476b81bad77a3e37fb9ecb136
skchandra <shivali.chandra@students.olin.edu>
2015-06-03 17:27:11 -0700

    Added more code for parsing sensor data and infrastructure for angular-map

--- app.js
+++ app.js
@@ -3,9 +3,35 @@ var express = require("express");
 var app = express();
 var request = require("request");
 var APIKEY = '76e253a5c51ecf1dbf17e9ea6b9d6a2f';
+var coordArray = [];
 
-app.get('/', function (req, resp) {
-  resp.send('hello world');
+function combine(callback, fxn) {
+  request('http://api.landscape-computing.com/nboxws/rest/v1/site/pa/query/summary/?key=' + APIKEY, function (error, response, body) {
+  
+  });
+}
+
+app.get('/api/gps', function (req, resp) {
+  request('http://api.landscape-computing.com/nboxws/rest/v1/zone/pa_2/?key=' + APIKEY, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      if (body.charAt(0) === "<") {
+        sensor = ;
+        coordinates = ;
+        coordArray[sensor] = coordinates;
+      } else {
+        data = JSON.parse(body).sensorId;
+
+        for (var i = 0; i < ; i++) {
+          sensor = data[i]["guid"];
+          coordinates = data[i]["gpsCoord"];
+          coordArray[sensor] = coordinates;
+        }
+      }
+    } else {
+      return console.log(error);
+    }
+    combine(coordArray);
+  } 
 });
 
-app.listen(3000);
+app.listen(process.env.PORT) || 8080;
--- visuals.js
+++ visuals.js
@@ -1,6 +1,11 @@
-var app = angular.module('myApplicationModule', ['uiGmapgoogle-maps']);
+var app = angular.module('myApp', ['uiGmapgoogle-maps']);
 
 app.controller('MapController', ['$scope', '$http', function ($scope, $http) {
   $scope.map = { center: { latitude: 37.427959, longitude: -122.134975 }, zoom: 19 };
+  
+  $http.get('/api/maps').success(function (data) {
+    $scope.sensors = data;
 
-  $http.get()
+  }
+
+}]);
